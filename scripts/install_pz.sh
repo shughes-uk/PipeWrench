eaa995a484d5d2d9b9883bd76768165b4c9ae710
Samantha Hughes <shughes.uk@gmail.com>
2022-08-20 16:25:35 -0700

    ignore ds_store

--- scripts/install_pz.sh
+++ scripts/install_pz.sh
@@ -2,5 +2,4 @@ set -e
 # mkdir -p steamcmd
 # curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_osx.tar.gz" | tar zxvf - --directory steamcmd
 # cd steamcmd
-steamcmd +runscript $PWD/scripts/install_pz.txt +quit
-cd ..
+steamcmd +force_install_dir $PWD/pzserver/ +login anonymous +app_update 380870 validate +quit
