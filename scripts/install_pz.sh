d88b50ee9e907c77605df9207ffd2f3db1260e07
Samantha Hughes <shughes.uk@gmail.com>
2022-08-20 16:12:48 -0700

    fix script

--- scripts/install_pz.sh
+++ scripts/install_pz.sh
@@ -2,5 +2,5 @@ set -e
 # mkdir -p steamcmd
 # curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_osx.tar.gz" | tar zxvf - --directory steamcmd
 # cd steamcmd
-steamcmd +runscript ./scripts/install_pz.txt
+steamcmd +runscript $PWD/scripts/install_pz.txt +quit
 cd ..
